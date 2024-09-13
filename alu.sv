3e408fbf752d280ff1f39eab614f44def1370b13
littleguac <123615242+littleguac@users.noreply.github.com>
2024-09-13 13:04:58 -0700

    alu.sv

--- alu.sv
+++ alu.sv
@@ -1,50 +1,32 @@
 
 module ALU(Ain,Bin,ALUop,out,Z);
-input [15:0] Ain, Bin;
-input [1:0] ALUop;
-output reg [15:0] out;
-output reg Z;
+input [15:0] Ain, Bin; //the two 16 bit signals that are being fed into the ALU 
+input [1:0] ALUop; //specifies which operation the ALU will perform	
+output reg [15:0] out;	//output of the ALU 
+output reg Z;	//zero flag
 
 	always_comb begin
 
 		if( ALUop == 2'b00 ) begin //specifies the addition operator
-		
-		out = Ain + Bin;
-
+			out = Ain + Bin;
 		end
-
 		else if ( ALUop == 2'b01 ) begin   //specifies the subtraction operator
-	
-		out = Ain - Bin;
-
+			out = Ain - Bin;
 		end
-
 		else if ( ALUop == 2'b10 ) begin //specifies the and operator
-	
-		out = Ain & Bin;
+			out = Ain & Bin;
+		end
+		else begin
+			out = ~Bin; //if none of the above are true, it must be the NOT operation in the ALU
+		end
 
+		if( out == 16'b0 ) begin //sets Zero flag if the output is 0 
+			Z = 1'b1;
 		end
 
 		else begin
-
-		out = ~Bin; //if none of the above are true, it must be the NOT operation in the ALU
-
+			Z = 1'b0; //if out is not zero, sets the zero flag to 0
 		end
-
-	
-
-		if( out == 16'b0000000000000000 ) begin //sets Z if all of the output are 0
-	
-		Z = 1'b1;
-
-		end
-
-		else begin
-
-		Z = 1'b0; //if out != 0, then Z stays at 0
-
-		end
-
 	end
 
 endmodule
